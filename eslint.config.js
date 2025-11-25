103b15f550368cd42dcc494ec6220387ac76dd7c
Otbat <lmartinez@win2tec.com>
2025-11-25 13:08:23 -0600

    fix: update ESLint config to use flat config array format
    - Replace deprecated tseslint.config() with direct array export
    - Remove disabled TypeScript ESLint rules to use default strict rules
    - Fix deprecation warning in ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default [
   js.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
@@ -12,14 +12,8 @@ export default tseslint.config(
         tsconfigRootDir: import.meta.dirname,
       },
     },
-    rules: {
-      "@typescript-eslint/interface-name-prefix": "off",
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/explicit-module-boundary-types": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-    },
   },
   {
     ignores: ["dist/**", "node_modules/**", "coverage/**", "*.config.js"],
   },
-);
+];
